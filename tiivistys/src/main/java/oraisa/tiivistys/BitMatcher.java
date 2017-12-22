17a9f974d460c329848ed1919e0bfe8218c9111a
Ossi Räisä <ossi.raisa@cs.helsinki.fi>
2017-12-22 19:34:37 +0200

    Make bit matcher work

--- tiivistys/src/main/java/oraisa/tiivistys/BitMatcher.java
+++ tiivistys/src/main/java/oraisa/tiivistys/BitMatcher.java
@@ -3,12 +3,48 @@ package oraisa.tiivistys;
 
 public class BitMatcher {
     private byte[] bytes;
+    private long bitPosition = 0;
 
     public BitMatcher(byte[] bytes){
         this.bytes = bytes;
     }
 
     public boolean matchBitPattern(BitPattern pattern){
-        return false;
+        int bits = pattern.getBitsInPattern();
+        for(int i = 0; i < bits; i++){
+            int bitPositionInPattern = 8 - (bits - i);
+            long bitPositionInData = i + bitPosition;
+            if(getBitAtBitPosition(bitPositionInData) !=
+            getBitAtPositionInByte(bitPositionInPattern, pattern.getPattern())){
+                return false;
+            }
+        }
+        bitPosition += bits;
+        return true;
+    }
+
+    private byte getBitAtBitPosition(long position){
+        if(position >= bytes.length * 8){
+            throw new ArrayIndexOutOfBoundsException("Position " + position + " is outside the data.");
+        } else if(position < 0){
+            throw new ArrayIndexOutOfBoundsException("Negative position");
+        }
+        byte byt = bytes[(int)(position / 8)];
+        return getBitAtPositionInByte(position % 8, byt);
+    }
+
+    private byte getBitAtPositionInByte(long position, byte byt){
+        if(position >= 8){
+            throw new ArrayIndexOutOfBoundsException("Position " + position + " >= 8.");
+        } else if(position < 0){
+            throw new ArrayIndexOutOfBoundsException("Negative position");
+        }
+        int mask = 1 << (8 - (position + 1));
+        int result = byt & mask;
+        if(result == 0){
+            return 0;
+        } else {
+            return 1;
+        }
     }
 }
