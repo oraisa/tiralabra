14939e5b39871978430868fc5e57d47eed310bd4
Ossi Räisä <ossi.raisa@cs.helsinki.fi>
2018-01-02 17:31:18 +0200

    Add code for compressing or uncompressing a given file to main

--- tiivistys/src/main/java/oraisa/tiivistys/Main.java
+++ tiivistys/src/main/java/oraisa/tiivistys/Main.java
@@ -1,19 +1,43 @@
 
 package oraisa.tiivistys;
+import java.util.*;
+import java.io.*;
+import java.nio.file.*;
 
 public class Main {
+    private static final String fileSuffix = ".tiiv";
     public static void main(String[] args){
-        System.out.println((byte)1 << 7);
         
-        System.out.println(Integer.toString(0xFF, 2));
-        byte byt = 4 + 8 + 2;
-        int position = 3;
-        int mask = 1 << (8 - (position + 1));
-        int result = byt & mask;
-        if(result == 0){
-            System.out.println("0");
+        if(args.length > 0){
+            String file = args[0];
+            if(file.endsWith(fileSuffix)){
+                unCompressFile(file);
+            } else {
+                compressFile(file);
+            }
         } else {
-            System.out.println("1");
+            System.err.println("No file name given.");
+        }
+    }
+    
+    private static void compressFile(String file){
+        try{
+            byte[] data = Files.readAllBytes(Paths.get(file));
+            CompressedFile compressedFile = CompressedFile.fromUnCompressedBytes(data);
+            Files.write(Paths.get(file + fileSuffix), compressedFile.getCompressedDataWithHeader());
+        } catch(IOException e){
+            System.err.println("Failed to read or write file");
+        }
+    }
+    
+    private static void unCompressFile(String file){
+        try{
+            byte[] data = Files.readAllBytes(Paths.get(file));
+            CompressedFile compressedFile = CompressedFile.fromCompressedBytes(data);
+            Files.write(Paths.get(file.substring(0, file.length() - fileSuffix.length())), 
+                    compressedFile.getUnCompressedData());
+        } catch(IOException e){
+            System.err.println("Failed to read or write file");
         }
     }
 }
