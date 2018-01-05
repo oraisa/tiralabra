fcbde6c61865bf9d0d321a03f576cac9ae8ced91
Ossi Räisä <ossi.raisa@cs.helsinki.fi>
2018-01-05 17:21:48 +0200

    Add tests for the heap and a test for the optimality of an the encoding obtained from an example

--- tiivistys/src/test/java/oraisa/tiivistys/HuffmanCodeCalculatorTest.java
+++ tiivistys/src/test/java/oraisa/tiivistys/HuffmanCodeCalculatorTest.java
@@ -23,18 +23,18 @@ public class HuffmanCodeCalculatorTest {
     
     Map<Byte, Long> exampleFrequencies;
     Map<Byte, Long> characterFrequenciesInFlippedBytes;
+    HuffmanTreeNodeHeap heap;
     @Before
     public void setUp() {
-        //This exaple is from http://math.mit.edu/~goemans/18310S15/huffman-notes.pdf
-        //The encoding length for 2 is 5 instead of the 4 in the example because
-        //the example doesn't include a stop code character.
+        //This exaple is from https://web.stanford.edu/class/archive/cs/cs106b/cs106b.1126/handouts/220%20Huffman%20Encoding.pdf
         exampleFrequencies = new HashMap<Byte, Long>();
-        exampleFrequencies.put((byte)1, 40L);
-        exampleFrequencies.put((byte)2, 5L);
-        exampleFrequencies.put((byte)3, 18L);
-        exampleFrequencies.put((byte)4, 7L);
-        exampleFrequencies.put((byte)5, 20L);
-        exampleFrequencies.put((byte)6, 10L);
+        exampleFrequencies.put((byte)1, 3L);//h
+        exampleFrequencies.put((byte)2, 1L);//a
+        exampleFrequencies.put((byte)3, 4L);//p
+        exampleFrequencies.put((byte)4, 1L);//y
+        exampleFrequencies.put((byte)5, 1L);//i
+        exampleFrequencies.put((byte)6, 2L);//space
+        //o represent the stop code
         
         
         byte[] flippedBytes = new byte[1000];
@@ -54,6 +54,8 @@ public class HuffmanCodeCalculatorTest {
                 characterFrequenciesInFlippedBytes.put(byt, 1L);
             }
         }
+        
+        heap = new HuffmanTreeNodeHeap(20);
     }
     
     @After
@@ -106,5 +108,64 @@ public class HuffmanCodeCalculatorTest {
     public void huffmanCodeCalculatorGiveAPrefixEncodingForFlippedData(){
         BitPattern[] huffmanCodes = HuffmanCodeCalculator.calculateHuffmanCodes(characterFrequenciesInFlippedBytes);
         testPrefixEncoding(huffmanCodes);
+    }
+    
+    @Test
+    public void huffmanCodeCalculatorGivesOptimalEncodingWithExample(){
+        BitPattern[] huffmanCodes = HuffmanCodeCalculator.calculateHuffmanCodes(exampleFrequencies);
+        int encodedLength = 0;
+        for(Byte key: exampleFrequencies.keySet()){
+            long freq = exampleFrequencies.get(key);
+            encodedLength += freq * huffmanCodes[key - Byte.MIN_VALUE].getBitsInPattern();
+        }
+        encodedLength += huffmanCodes[huffmanCodes.length - 1].getBitsInPattern();
+        assertEquals(34, encodedLength);
+    }
+    
+    @Test
+    public void huffmanTreeNodeHeapReturnsValueAfterInsert(){
+        heap.insert(new HuffmanTreeNode((byte)0, 5));
+        assertEquals(5, heap.poll().getFrequency());
+    }
+    
+    @Test
+    public void huffmanTreeNodeHeapReturnsSmallerValueAfterTwoInserts(){
+        heap.insert(new HuffmanTreeNode((byte)0, 10));
+        heap.insert(new HuffmanTreeNode((byte)0, 4));
+        assertEquals(4, heap.poll().getFrequency());
+    }
+    
+    @Test
+    public void huffmanTreeNodeHeapReturnsValuesInCorrectOrderAferManyInserts(){
+        heap.insert(new HuffmanTreeNode((byte)0, 5));
+        heap.insert(new HuffmanTreeNode((byte)0, 3));
+        heap.insert(new HuffmanTreeNode((byte)0, 7));
+        heap.insert(new HuffmanTreeNode((byte)0, 200));
+        heap.insert(new HuffmanTreeNode((byte)0, 34));
+        heap.insert(new HuffmanTreeNode((byte)0, 1));
+        assertEquals(1, heap.poll().getFrequency());
+        assertEquals(3, heap.poll().getFrequency());
+        assertEquals(5, heap.poll().getFrequency());
+        assertEquals(7, heap.poll().getFrequency());
+        assertEquals(34, heap.poll().getFrequency());
+        assertEquals(200, heap.poll().getFrequency());
+    }
+    
+    @Test
+    public void huffmanTreeNodeHeapReturnsValuesInCorrectOrderAferManyInsertsOutOfOrder(){
+        heap.insert(new HuffmanTreeNode((byte)0, 5));//5
+        heap.insert(new HuffmanTreeNode((byte)0, 3));//3, 5
+        heap.insert(new HuffmanTreeNode((byte)0, 7));//3, 5, 7
+        assertEquals(3, heap.poll().getFrequency());//5, 7
+        heap.insert(new HuffmanTreeNode((byte)0, 34));//5, 7, 34
+        heap.insert(new HuffmanTreeNode((byte)0, 20));//5, 7, 20, 34
+        assertEquals(5, heap.poll().getFrequency());//7, 20, 34
+        assertEquals(7, heap.poll().getFrequency());//20, 34
+        heap.insert(new HuffmanTreeNode((byte)0, 10));//10, 20, 34
+        assertEquals(10, heap.poll().getFrequency());//20, 34
+        heap.insert(new HuffmanTreeNode((byte)0, 7));//7. 20, 34
+        assertEquals(7, heap.poll().getFrequency());//20, 34
+        assertEquals(20, heap.poll().getFrequency());//34
+        assertEquals(34, heap.poll().getFrequency());
     }
 }
