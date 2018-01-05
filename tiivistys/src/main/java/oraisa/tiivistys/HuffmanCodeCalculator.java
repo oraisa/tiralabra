ae1045c21e1575c127b4e02c2c16277301b7b675
Ossi Räisä <ossi.raisa@cs.helsinki.fi>
2018-01-05 16:30:58 +0200

    Add minimum heap implementation for HuffmanCodeCalculator

--- tiivistys/src/main/java/oraisa/tiivistys/HuffmanCodeCalculator.java
+++ tiivistys/src/main/java/oraisa/tiivistys/HuffmanCodeCalculator.java
@@ -1,7 +1,7 @@
 
 package oraisa.tiivistys;
 
-import java.util.*;
+import java.util.Map;
 
 /**
  * Contains the algorithm to calculate the Huffman encoding for a given 
@@ -27,17 +27,18 @@ public class HuffmanCodeCalculator {
         int characters = 256;
         BitPattern[] huffmanCodes = new BitPattern[characters + 1];
         
-        PriorityQueue<HuffmanTreeNode> nodes = new PriorityQueue<HuffmanTreeNode>();
+        //PriorityQueue<HuffmanTreeNode> nodes = new PriorityQueue<HuffmanTreeNode>();
+        HuffmanTreeNodeHeap nodes = new HuffmanTreeNodeHeap(huffmanCodes.length);
         for(Byte character: characterFrequencies.keySet()){
-            nodes.offer(new HuffmanTreeNode(character, characterFrequencies.get(character)));
+            nodes.insert(new HuffmanTreeNode(character, characterFrequencies.get(character)));
         }
-        nodes.offer(HuffmanTreeNode.createStopCode());
+        nodes.insert(HuffmanTreeNode.createStopCode());
         
         while(nodes.size() > 1){
             HuffmanTreeNode minimumNode = nodes.poll();
             HuffmanTreeNode secondMinimumNode = nodes.poll();
             HuffmanTreeNode newNode = new HuffmanTreeNode(minimumNode, secondMinimumNode);
-            nodes.offer(newNode);
+            nodes.insert(newNode);
         }
         
         traverseHuffmanTree(huffmanCodes, nodes.peek(), new BitPattern(0, 0, 0));
@@ -102,5 +103,78 @@ class HuffmanTreeNode implements Comparable<HuffmanTreeNode> {
     @Override
     public int compareTo(HuffmanTreeNode o) {
         return new Long(frequency).compareTo(o.getFrequency());
+    }
+}
+
+class HuffmanTreeNodeHeap {
+    
+    private HuffmanTreeNode[] heapArray;
+    private int heapSize;
+    
+    private int parentIndex(int i){
+        return i / 2;
+    }
+    private int leftChildIndex(int i){
+        return 2 * i;
+    }
+    private int rightChildIndex(int i){
+        return 2 * i + 1;
+    }
+    
+    public HuffmanTreeNodeHeap(int maxSize){
+        heapArray = new HuffmanTreeNode[maxSize];
+        heapSize = 0;
+    }
+    
+    private void heapify(int index){
+        int rightIndex = rightChildIndex(index);
+        int leftIndex = leftChildIndex(index);
+        if(rightIndex < heapSize){
+            int minimumChildIndex;
+            if(heapArray[leftIndex].compareTo(heapArray[rightIndex]) < 0){
+                minimumChildIndex = leftIndex;
+            } else {
+                minimumChildIndex = rightIndex;
+            }
+            if(heapArray[index].compareTo(heapArray[minimumChildIndex]) > 0){
+                swap(index, minimumChildIndex);
+                heapify(minimumChildIndex);
+            }
+        } else if(leftIndex == heapSize - 1 && heapArray[index].compareTo(heapArray[leftIndex]) > 0){
+            swap(index, leftIndex);
+        }
+    }
+    
+    private void swap(int i, int j){
+        HuffmanTreeNode temp = heapArray[i];
+        heapArray[i] = heapArray[j];
+        heapArray[j] = temp;
+    }
+    
+    public HuffmanTreeNode peek(){
+        return heapArray[0];
+    }
+    
+    public HuffmanTreeNode poll(){
+        HuffmanTreeNode minNode = heapArray[0];
+        heapArray[0] = heapArray[heapSize - 1];
+        heapArray[heapSize - 1] = null;
+        heapSize -= 1;
+        heapify(0);
+        return minNode;
+    }
+    
+    public void insert(HuffmanTreeNode node){
+        heapSize += 1;
+        int i = heapSize - 1;
+        while(i > 1 && heapArray[parentIndex(i)].compareTo(node) > 0){
+            heapArray[i] = heapArray[parentIndex(i)];
+            i = parentIndex(i);
+        }
+        heapArray[i] = node;
+    }
+    
+    public int size(){
+        return heapSize;
     }
 }
