67b725d9b7f49ab2f01b1bb2136e6c5ca03f0321
Ossi Räisä <ossi.raisa@cs.helsinki.fi>
2018-01-08 16:05:36 +0200

    Add a StopWatch class for measuring time

---
+++ tiivistys/src/main/java/oraisa/tiivistys/StopWatch.java
@@ -0,0 +1,51 @@
+
+package oraisa.tiivistys;
+
+/**
+ * A stop watch for measuring time. Uses System.currentTimeMillis to measure 
+ * time.
+ */
+public class StopWatch {
+    
+    private long startTime;
+    private long stopTime;
+    private boolean running = false;
+    
+    /**
+     * Start the stop watch.
+     */
+    public void start(){
+        startTime = System.currentTimeMillis();
+        running = true;
+    }
+    
+    /**
+     * Stop the stop watch.
+     */
+    public void stop(){
+        stopTime = System.currentTimeMillis();
+        running = false;
+    }
+    
+    /**
+     * Return the elapsed time. If this stop watch is running, return the time
+     * elapsed between the last call to start and present. If this is not 
+     * running, return the time between the latest calls to start and stop.
+     * @return The elapsed time in milliseconds.
+     */
+    public long getElapsedTime(){
+        if(isRunning()){
+            return System.currentTimeMillis() - startTime;
+        } else {
+            return stopTime - startTime;
+        }
+    }
+    
+    /**
+     * Is this stop watch running.
+     * @return Is this stop watch running.
+     */
+    public boolean isRunning(){
+        return running;
+    }
+}
