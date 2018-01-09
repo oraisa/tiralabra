212a3bfdf97fa5b75562a7a58be04bd82f8425d2
Ossi Räisä <ossi.raisa@cs.helsinki.fi>
2018-01-09 17:14:06 +0200

    Create a class for reading binary data one bit at a time

---
+++ tiivistys/src/main/java/oraisa/tiivistys/BitInputStream.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package oraisa.tiivistys;
+
+/**
+ * An input stream read one bit at a time. The bits are read from an underlying 
+ * byte array specified by the constructor.
+ */
+public class BitInputStream {
+    
+    private byte[] bytes;
+    private long bitPosition = 0;
+
+    /**
+     * Class constructor.
+     * @param bytes An array of bytes representing the binary data that is read from.
+     */
+    public BitInputStream(byte[] bytes){
+        this.bytes = bytes;
+    }
+    
+    /**
+     * Return how many bits of data are left.
+     * @return The amount of bits left in the data.
+     */
+    public long bitsLeft(){
+        return bytes.length * 8 - bitPosition;
+    }
+
+    private byte getBitAtBitPosition(long position){
+        if(position >= bytes.length * 8){
+            throw new IllegalArgumentException("Position " + position + " is outside the data.");
+        } else if(position < 0){
+            throw new IllegalArgumentException("Negative position");
+        }
+        byte byt = bytes[(int)(position / 8)];
+        return getBitAtPositionInShort(position % 8 + 8, (short)byt);
+    }
+
+    //TODO: this can probably be optimised
+    private byte getBitAtPositionInShort(long position, short shor){
+        if(position >= 16){
+            throw new IllegalArgumentException("Position " + position + " >= 16.");
+        } else if(position < 0){
+            throw new IllegalArgumentException("Negative position");
+        }
+        int mask = 1 << (16 - (position + 1));
+        int result = shor & mask;
+        if(result == 0){
+            return 0;
+        } else {
+            return 1;
+        }
+    }
+    
+    /**
+     * Read a bit from the underlying data.
+     * @return The next bit of data.
+     */
+    public int readBit(){
+        return getBitAtBitPosition(bitPosition++);
+    }
+}
---
+++ tiivistys/src/test/java/oraisa/tiivistys/BitInputStreamTest.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package oraisa.tiivistys;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class BitInputStreamTest {
+    
+    public BitInputStreamTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void correctBitReadFromBitInputStreamWithOneByte(){
+        BitInputStream stream = new BitInputStream(new byte[]{2 + 64 + 8});
+        assertEquals(0, stream.readBit());
+        assertEquals(1, stream.readBit());
+        assertEquals(0, stream.readBit());
+        assertEquals(0, stream.readBit());
+        assertEquals(1, stream.readBit());
+        assertEquals(0, stream.readBit());
+        assertEquals(1, stream.readBit());
+        assertEquals(0, stream.readBit());
+    }
+
+    @Test
+    public void correctBitReadFromBitInputStreamWithTwoBytes(){
+        BitInputStream stream = new BitInputStream(new byte[]{2 + 64 + 8, 4 + 2 + 32});
+        assertEquals(0, stream.readBit());
+        assertEquals(1, stream.readBit());
+        assertEquals(0, stream.readBit());
+        assertEquals(0, stream.readBit());
+        assertEquals(1, stream.readBit());
+        assertEquals(0, stream.readBit());
+        assertEquals(1, stream.readBit());
+        assertEquals(0, stream.readBit());
+        
+        assertEquals(0, stream.readBit());
+        assertEquals(0, stream.readBit());
+        assertEquals(1, stream.readBit());
+        assertEquals(0, stream.readBit());
+        assertEquals(0, stream.readBit());
+        assertEquals(1, stream.readBit());
+        assertEquals(1, stream.readBit());
+        assertEquals(0, stream.readBit());
+    }
+}
