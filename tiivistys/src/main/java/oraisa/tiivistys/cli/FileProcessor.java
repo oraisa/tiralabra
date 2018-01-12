afd7a1e8582164df9494568b273084c25665e92e
Ossi Räisä <ossi.raisa@cs.helsinki.fi>
2018-01-12 17:01:18 +0200

    Extract compressFile and unComprressFile methods the their own class

---
+++ tiivistys/src/main/java/oraisa/tiivistys/cli/FileProcessor.java
@@ -0,0 +1,64 @@
+
+package oraisa.tiivistys.cli;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import oraisa.tiivistys.logic.CompressedFile;
+import oraisa.tiivistys.measuring.ActiveMeasurer;
+
+public class FileProcessor {
+    
+    public static final String fileSuffix = ".tiiv";
+    
+    public static void compressFile(String file, String outputFile){
+        try{
+            ActiveMeasurer.getMeasurer().startReadingFile();
+            byte[] data = Files.readAllBytes(Paths.get(file));
+            ActiveMeasurer.getMeasurer().endReadingFile();
+            ActiveMeasurer.getMeasurer().reportFileSizeBeforeCompression(data.length);
+            
+            CompressedFile compressedFile = CompressedFile.fromUnCompressedBytes(data);
+            byte[] compressedData = compressedFile.getCompressedDataWithHeader();
+            ActiveMeasurer.getMeasurer().reportFileSizeAfterCompression(compressedData.length);
+            
+            ActiveMeasurer.getMeasurer().startWritingFile();
+            Path outPath;
+            if(outputFile.equals("")){
+                outPath = Paths.get(file + fileSuffix);
+            } else {
+                outPath = Paths.get(outputFile);
+            }
+            Files.write(outPath, compressedData);
+            ActiveMeasurer.getMeasurer().endWritingFile();
+        } catch(IOException e){
+            System.err.println(e.getLocalizedMessage());
+        }
+    }
+    
+    public static void unCompressFile(String file, String outputFile){
+        try{
+            ActiveMeasurer.getMeasurer().startReadingFile();
+            byte[] data = Files.readAllBytes(Paths.get(file));
+            ActiveMeasurer.getMeasurer().endReadingFile();
+            
+            CompressedFile compressedFile = CompressedFile.fromCompressedBytes(data);
+            byte[] unCompressedData = compressedFile.getUnCompressedData();
+            
+            ActiveMeasurer.getMeasurer().startWritingFile();
+            Path outPath;
+            if(outputFile.equals("")){
+                outPath = Paths.get(file.substring(0, file.length() - fileSuffix.length()));
+            } else {
+                outPath = Paths.get(outputFile);
+            }
+            Files.write(outPath, unCompressedData);
+            ActiveMeasurer.getMeasurer().endWritingFile();
+        } catch(IOException e){
+            System.err.println(e.getLocalizedMessage());
+        }
+    }
+    
+    private FileProcessor(){}
+}
--- tiivistys/src/main/java/oraisa/tiivistys/cli/Main.java
+++ tiivistys/src/main/java/oraisa/tiivistys/cli/Main.java
@@ -1,12 +1,8 @@
 
 package oraisa.tiivistys.cli;
-import java.io.*;
-import java.nio.file.*;
-import oraisa.tiivistys.logic.CompressedFile;
 import oraisa.tiivistys.measuring.ActiveMeasurer;
 
 public class Main {
-    private static final String fileSuffix = ".tiiv";
     
     public static void main(String[] args){
         
@@ -28,13 +24,13 @@ public class Main {
             if(includeTiming){
                 ActiveMeasurer.setActiveMeasurer(new StopWatchMeasurer());
             }
-            if(file.endsWith(fileSuffix)){
+            if(file.endsWith(FileProcessor.fileSuffix)){
                 ActiveMeasurer.getMeasurer().startEntireProcess();
-                unCompressFile(file, outputFile);
+                FileProcessor.unCompressFile(file, outputFile);
                 ActiveMeasurer.getMeasurer().endEntireProcess();
             } else {
                 ActiveMeasurer.getMeasurer().startEntireProcess();
-                compressFile(file, outputFile);
+                FileProcessor.compressFile(file, outputFile);
                 ActiveMeasurer.getMeasurer().endEntireProcess();
             }
             if(includeTiming){
@@ -45,51 +41,4 @@ public class Main {
         }
     }
     
-    private static void compressFile(String file, String outputFile){
-        try{
-            ActiveMeasurer.getMeasurer().startReadingFile();
-            byte[] data = Files.readAllBytes(Paths.get(file));
-            ActiveMeasurer.getMeasurer().endReadingFile();
-            ActiveMeasurer.getMeasurer().reportFileSizeBeforeCompression(data.length);
-            
-            CompressedFile compressedFile = CompressedFile.fromUnCompressedBytes(data);
-            byte[] compressedData = compressedFile.getCompressedDataWithHeader();
-            ActiveMeasurer.getMeasurer().reportFileSizeAfterCompression(compressedData.length);
-            
-            ActiveMeasurer.getMeasurer().startWritingFile();
-            Path outPath;
-            if(outputFile.equals("")){
-                outPath = Paths.get(file + fileSuffix);
-            } else {
-                outPath = Paths.get(outputFile);
-            }
-            Files.write(outPath, compressedData);
-            ActiveMeasurer.getMeasurer().endWritingFile();
-        } catch(IOException e){
-            System.err.println(e.getLocalizedMessage());
-        }
-    }
-    
-    private static void unCompressFile(String file, String outputFile){
-        try{
-            ActiveMeasurer.getMeasurer().startReadingFile();
-            byte[] data = Files.readAllBytes(Paths.get(file));
-            ActiveMeasurer.getMeasurer().endReadingFile();
-            
-            CompressedFile compressedFile = CompressedFile.fromCompressedBytes(data);
-            byte[] unCompressedData = compressedFile.getUnCompressedData();
-            
-            ActiveMeasurer.getMeasurer().startWritingFile();
-            Path outPath;
-            if(outputFile.equals("")){
-                outPath = Paths.get(file.substring(0, file.length() - fileSuffix.length()));
-            } else {
-                outPath = Paths.get(outputFile);
-            }
-            Files.write(outPath, unCompressedData);
-            ActiveMeasurer.getMeasurer().endWritingFile();
-        } catch(IOException e){
-            System.err.println(e.getLocalizedMessage());
-        }
-    }
 }
